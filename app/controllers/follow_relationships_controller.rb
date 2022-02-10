afa94b63b22a41a39fd4f6ab8b0456aebd3824b0
lan <lanlehoang10@gmail.com>
2022-02-10 20:30:47 +0700

    remove respond_to

--- app/controllers/follow_relationships_controller.rb
+++ app/controllers/follow_relationships_controller.rb
@@ -8,15 +8,8 @@ class FollowRelationshipsController < ApplicationController
     return unless @user
 
     @active_relationship = current_user.follow(@user)
-    respond_to do |format|
-      if @active_relationship.save
-        format.html { redirect_to @user, notice: t('controllers.common.notice_create', name: User.model_name.human) }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { redirect_to @user }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    @active_relationship.save
+    redirect_to @user, notice: t('controllers.common.notice_create', name: User.model_name.human)
   end
 
   def destroy
@@ -24,14 +17,7 @@ class FollowRelationshipsController < ApplicationController
     return unless @user
 
     @passive_relationship = current_user.unfollow(@user)
-    respond_to do |format|
-      if @passive_relationship.destroy
-        format.html { redirect_to @user, notice: t('controllers.common.notice_destroy', name: User.model_name.human) }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { redirect_to @user }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    @passive_relationship.destroy
+    redirect_to @user, notice: t('controllers.common.notice_destroy', name: User.model_name.human) 
   end
 end
